0b68329eb6f475408beb61b65ac747c094611266
caruazu <gustavocaruazu@gmail.com>
2025-03-20 22:31:29 -0300

    esbolço da comunicação com a api ao fazer login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,6 +3,7 @@ import { LayoutLoginComponent } from "../../components/layout-login/layout-login
 import { FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
+import { CommonModule } from '@angular/common';
 
 interface LoginForm {
   username: FormControl;
@@ -11,12 +12,14 @@ interface LoginForm {
 
 @Component({
   selector: 'app-login',
-  imports: [LayoutLoginComponent, ReactiveFormsModule],
+  imports: [LayoutLoginComponent, ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
   loginForm!: FormGroup<LoginForm>;
+
+  loginResponse: any;
 
   constructor(private router: Router, private loginService: LoginService) {
     this.loginForm = new FormGroup({
@@ -27,7 +30,16 @@ export class LoginComponent {
 
   onSubmit() {
     const values = this.loginForm.value;
-    this.loginService.entrar(values.username, values.password);
+    this.loginService.entrar(values.username, values.password).subscribe({
+      next: (response) => {
+        console.log(response.timestamp)
+        this.loginResponse = response.message;
+      },
+      error: (err) => {
+        console.log(err.error?.timestamp);
+        this.loginResponse = err.error?.message || 'Ocorreu um erro desconhecido.';
+      },
+    });
   }
 
   navigate() {
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,14 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, map, Observable, of, tap } from 'rxjs';
+
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class LoginService {
-  constructor() {}
+  apiUrl: string = 'http://localhost:8080';
 
-  entrar(username: string, password: string) {
-    console.log(username);
-    console.log(password);
+  constructor(private httpClient: HttpClient) {}
+
+  entrar(username: string, password: string): Observable<any> {
+    return this.httpClient.post(this.apiUrl + '/auth/login', {username,password,})
   }
 
   cadastro(username: string, email: string, password: string) {
